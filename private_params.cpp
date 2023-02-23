470cf6912588cb1d9df8114ad667e2130f93f950
Larysa Zaremba <larysa.zaremba@gmail.com>
2023-02-23 23:31:45 +0100

    normalize private parameter scrolling speed

--- private_params.cpp
+++ private_params.cpp
@@ -18,13 +18,19 @@ void show_private_params(system_nd *system)
         return;
 
     ImGui::Begin("System parameters");
-    for (uint i = 0; i < num_f; i++)
-        ImGui::DragFloat(info_f[i].name.c_str(), params_f[i], 1.f,
+    for (uint i = 0; i < num_f; i++) {
+        float speed = info_f[i].range.max - info_f[i].range.min;
+        speed /= 40.f;
+        ImGui::DragFloat(info_f[i].name.c_str(), params_f[i], speed,
                          info_f[i].range.min, info_f[i].range.max);
+    }
 
-    for (uint i = 0; i < num_i; i++)
-        ImGui::DragInt(info_i[i].name.c_str(), params_i[i], 1.f,
+    for (uint i = 0; i < num_i; i++) {
+        float speed = (float)info_f[i].range.max - (float)info_f[i].range.min;
+        speed /= 40.f;
+        ImGui::DragInt(info_i[i].name.c_str(), params_i[i], speed,
                        info_i[i].range.min, info_i[i].range.max);
+    }
     ImGui::End();
     
 }
